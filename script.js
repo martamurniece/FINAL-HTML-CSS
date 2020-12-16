1ef1578ec0b834ee5651d978b8dc338e5908dcd2
martamurniece <45332486+martamurniece@users.noreply.github.com>
2020-12-16 13:01:36 +0100

    FINAL-HTML-CSS

---
+++ script.js
@@ -0,0 +1,10 @@
+function openContact() {
+    document.querySelector("aside").classList.add("active");
+}
+function closeContact() {
+    document.querySelector("aside").classList.remove("active");
+    
+}
+function goBack(){
+    window.history.back();
+}
